21557acc0eafe437b00b22869fa8f63241f956e0
joe <krasherjoe@gmail.com>
2026-04-28 21:00:52 +0900

    角印Xオフセット方向修正（+で右移動、-で左移動）

--- lib/services/pdf_generator.dart
+++ lib/services/pdf_generator.dart
@@ -56,11 +56,15 @@ Future<pw.Document> buildInvoiceDocument(
           boldItalic: ipaex,
         ).copyWith(defaultTextStyle: pw.TextStyle(fontFallback: [ipaex])),
         buildBackground: (context) {
+          final pageWidth = PdfPageFormat.a4.width;
+          final sealWidth = 100.0;
+          final margin = 32.0;
+          final maxRightOffset = pageWidth - (margin * 2) - sealWidth;
           return pw.Stack(
             children: [
               if (sealImage != null)
                 pw.Positioned(
-                  right: sealOffsetXOverride ?? companyInfo.sealOffsetX,
+                  right: maxRightOffset - (sealOffsetXOverride ?? companyInfo.sealOffsetX),
                   top: sealOffsetYOverride ?? companyInfo.sealOffsetY,
                   child: pw.Transform.rotate(
                     angle: (sealRotationOverride ?? companyInfo.sealRotation) * math.pi / 180,
