e8fbb8f2632b0930eb3bae3cf85fe160fca2647c
joe <krasherjoe@gmail.com>
2026-05-20 16:42:41 +0900

    D3伝票入力画面の背景を2段階暗く調整（surfaceContainerHighに変更）

--- lib/screens/invoice_input_screen.dart
+++ lib/screens/invoice_input_screen.dart
@@ -704,7 +704,7 @@ class _InvoiceInputFormState extends State<InvoiceInputForm> {
   @override
   Widget build(BuildContext context) {
     final fmt = NumberFormat("#,###");
-    final themeColor = Theme.of(context).scaffoldBackgroundColor;
+    final themeColor = Theme.of(context).colorScheme.surfaceContainerHigh;
     final isDark = Theme.of(context).brightness == Brightness.dark;
     final textColor = Theme.of(context).colorScheme.onSurface;
 
